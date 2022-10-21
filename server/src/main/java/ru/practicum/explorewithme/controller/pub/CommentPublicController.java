d94dc85d49aee12d3e05312b4de054816235d8b6
Kate Mahova <kate.mahova@yandex.ru>
2022-10-21 14:36:51 +0300

    fix: code style

--- server/src/main/java/ru/practicum/explorewithme/controller/pub/CommentPublicController.java
+++ server/src/main/java/ru/practicum/explorewithme/controller/pub/CommentPublicController.java
@@ -2,7 +2,6 @@ package ru.practicum.explorewithme.controller.pub;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.explorewithme.client.StatsClient;
 import ru.practicum.explorewithme.dto.comment.CommentDto;
 import ru.practicum.explorewithme.service.pub.CommentPublicService;
 
